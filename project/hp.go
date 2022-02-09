5f4e014da558309e6581ef8af133156e00eb73fb
Ben Sullivan <sullivanb23@users.noreply.github.com>
2022-02-09 20:44:01 +0000

    updates

--- project/hp.go
+++ project/hp.go
@@ -7,6 +7,7 @@ import (
     "log"
     "net/http"
     "os"
+	// "strconv"
 )
 
 
@@ -15,35 +16,62 @@ import (
 // http://hp-api.herokuapp.com/api/characters/staff - view all characters who are Hogwarts staff during the book series
 // http://hp-api.herokuapp.com/api/characters/house/:house - view all characters in a certain house, e.g. /slytherin
 
-
+// Setup structure
 type Details []struct {
-        Name           string        `json:"name, omitempty"`
-        AlternateNames []interface{} `json:"alternate_names, omitempty"`
-        Species        string        `json:"species, omitempty"`
-        Gender         string        `json:"gender, omitempty"`
-        House          string        `json:"house, omitempty"`
-        DateOfBirth    string        `json:"dateOfBirth, omitempty"`
-        YearOfBirth    int           `json:"yearOfBirth, omitempty"`
-        Wizard         bool          `json:"wizard, omitempty"`
-        Ancestry       string        `json:"ancestry, omitempty"`
-        EyeColour      string        `json:"eyeColour, omitempty"`
-        HairColour     string        `json:"hairColour, omitempty"`
-        Wand           struct {
-                Wood   string `json:"wood, omitempty"`
-                Core   string `json:"core, omitempty"`
-                Length int    `json:"length, omitempty"`
-        } `json:"wand"`
-        Patronus        string        `json:"patronus, omitempty"`
-        HogwartsStudent bool          `json:"hogwartsStudent, omitempty"`
-        HogwartsStaff   bool          `json:"hogwartsStaff, omitempty"`
-        Actor           string        `json:"actor, omitempty"`
-        AlternateActors []interface{} `json:"alternate_actors, omitempty"`
-        Alive           bool          `json:"alive, omitempty"`
-        Image           string        `json:"image, omitempty"`
+	Name           string        `json:"name"`
+	AlternateNames []interface{} `json:"alternate_names"`
+	// AlternateNames []string 	 `json:"alternate_names"`
+	Species        string        `json:"species"`
+	Gender         string        `json:"gender"`
+	House          string        `json:"house"`
+	DateOfBirth    string        `json:"dateOfBirth"`
+	YearOfBirth    int        	 `json:"yearOfBirth"`
+	Wizard         bool          `json:"wizard"`
+	Ancestry       string        `json:"ancestry"`
+	EyeColour      string        `json:"eyeColour"`
+	HairColour     string        `json:"hairColour"`
+	Wand           struct {
+		Wood   string `json:"wood"`
+		Core   string `json:"core"`
+		Length int    `json:"length"`
+	} `json:"wand"`
+	Patronus        string        `json:"patronus"`
+	HogwartsStudent bool          `json:"hogwartsStudent"`
+	HogwartsStaff   bool          `json:"hogwartsStaff"`
+	Actor           string        `json:"actor"`
+	AlternateActors []interface{} `json:"alternate_actors"`
+	Alive           bool          `json:"alive"`
+	Image           string        `json:"image"`
 }
+
+// func getdata(){
+//     response, err := http.Get("http://hp-api.herokuapp.com/api/characters/")
+
+//     if err != nil {
+//         fmt.Print(err.Error())
+//         os.Exit(1)
+//     }
+
+//     body, err := ioutil.ReadAll(response.Body)
+//     if err != nil {
+//         log.Fatal(err)
+//     }
+//     // fmt.Println(string(body))
+
+//     var responseObject Details
+//     json.Unmarshal(body, &responseObject)
+
+// }
+
+// func writedata(){
+//     for i, s := range responseObject {
+//         fmt.Println("Position", i, "contains the string:", s)
+//     }
+// }
 
 
 func main() {
+
     response, err := http.Get("http://hp-api.herokuapp.com/api/characters/")
 
     if err != nil {
@@ -59,5 +87,27 @@ func main() {
 
     var responseObject Details
     json.Unmarshal(body, &responseObject)
--- INSERT --                                                                                                                                                                                                                                                1,1           Top
-[ttyd] 0:vim*                                                                                                                                                                                                                          "student@bchd: ~/mycod" 16:29 09-Feb-22
+
+	// for i, s := range responseObject {
+    //     fmt.Println("Position", i, "contains the string:", s)
+    // }
+
+	for i := 0; i < len(responseObject); i++ {
+        fmt.Println("Character Name: " + responseObject[i].Name)
+        // fmt.Println("Alternate Name: " + responseObject[i].AlternateNames)
+        fmt.Println("Species: " + responseObject[i].Species)
+		fmt.Println("Gender: " + responseObject[i].Gender)
+		fmt.Println("House: " + responseObject[i].House)
+		fmt.Println("DateOfBirth: " + responseObject[i].DateOfBirth)
+		// fmt.Println("YearOfBirth: " + responseObject[i].YearOfBirth)
+		// fmt.Println("Wizard: " + responseObject[i].Wizard)
+		fmt.Println("Ancestry: " + responseObject[i].Ancestry)
+		fmt.Println("EyeColour: " + responseObject[i].EyeColour)
+		fmt.Println("HairColour: " + responseObject[i].HairColour)
+		fmt.Println("Wand Wood Type: " + responseObject[i].Wand.Wood)
+		fmt.Println("Wand Core Type: " + responseObject[i].Wand.Core)
+		fmt.Println("Wand Length: " + responseObject[i].Wand.Length)
+
+	}
+
+}
